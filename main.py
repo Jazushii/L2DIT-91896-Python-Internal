7b5c4163259270a6d4d8df56070be997dd9c642d
Jazushii <jazushii7@gmail.com>
2025-08-07 12:59:07 +1200

    lists
    added lists that contain the survey questions and its respective entry's name
    also added the grid configurations

--- main.py
+++ main.py
@@ -3,4 +3,27 @@ import random as rdm
 
 wdnw_srvy = tk.Tk()
 
+wdnw_srvy.rowconfigure([0, 1, 2, 3, 4, 5, 6], minsize=25)
+wdnw_srvy.columnconfigure([0, 1], minsize=25)
+
+list_qstn = {
+    "Given name:" : "0",
+    "Middle name:" : "1",
+    "Family name:" : "2",
+    "Item name:" : "3",
+    "Quantity of items:" : "4",
+    "Date the item/s will be issued:" : "5",
+    "Data the item/s will be returned" : "6",
+}
+
+list_inpt = {
+    "nm_gvn" : "0",
+    "nm_mid" : "1",
+    "nm_lst" : "2",
+    "itm_nm" : "3",
+    "itm_num" : "4",
+    "itm_issued" : "5",
+    "itm_returned" : "6",
+}
+
 wdnw_srvy.mainloop()
